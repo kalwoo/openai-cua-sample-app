9d361ff4e3d47ab7813e1d7cc56667bd1c71528e
kalwoo <kaladanwuke@galini.ai>
2025-06-10 12:38:33 -0700

    added docker commands to utils

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from PIL import Image
 from io import BytesIO
 import io
 from urllib.parse import urlparse
+import subprocess
 
 load_dotenv(override=True)
 
@@ -74,3 +75,19 @@ def check_blocklisted_url(url: str) -> None:
         for blocked in BLOCKED_DOMAINS
     ):
         raise ValueError(f"Blocked URL: {url}")
+    
+## docker commands
+def docker_exec(cmd: str, container_name: str, decode=True) -> str:
+    safe_cmd = cmd.replace('"', '\"')
+    docker_cmd = f'docker exec {container_name} sh -c "{safe_cmd}"'
+    output = subprocess.check_output(docker_cmd, shell=True)
+    if decode:
+        return output.decode("utf-8", errors="ignore")
+    return output
+
+class VM:
+    def __init__(self, display, container_name):
+        self.display = display
+        self.container_name = container_name
+
+vm = VM(display=":99", container_name="cua-image")
